fbc667f9e33483c3d81ee35824427ebe9dcc4f4f
Samuel Sackey <smlscky@gmail.com>
2021-10-31 15:45:34 +0000

    Changed Keys

--- fb.js
+++ fb.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdVkCpW1LBQGLQO4IYNnhevG1gftR5kJc",
-  authDomain: "solodebschat-837fe.firebaseapp.com",
-  projectId: "solodebschat-837fe",
-  storageBucket: "solodebschat-837fe.appspot.com",
-  messagingSenderId: "808102716299",
-  appId: "1:808102716299:web:1f089d3a13c549e0c3058e",
+  apiKey: "AIzaSyCB10k221mwuyx3XXZg0P_5DmBDnWF8mX8",
+  authDomain: "solodebschat-f53ef.firebaseapp.com",
+  projectId: "solodebschat-f53ef",
+  storageBucket: "solodebschat-f53ef.appspot.com",
+  messagingSenderId: "844959907038",
+  appId: "1:844959907038:web:791a956fcbf6ddd852d64f",
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
